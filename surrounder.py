a199ddb44d48964804958ce3a4184c59c0d38bc0
Andrea Serrano Urea <0andreasu@gmail.com>
2018-04-12 18:34:54 +0200

    first commit

---
+++ surrounder.py
@@ -0,0 +1,21 @@
+import sublime
+import sublime_plugin
+
+class PromptSurrounderCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.show_input_panel("Surround by:", "", self.on_done, None, None)
+
+    def on_done(self, tag):
+        try:
+            if self.window.active_view():
+                self.window.active_view().run_command("surround_by", {"tag": tag})
+        except ValueError:
+            pass
+
+
+class SurroundByCommand(sublime_plugin.TextCommand):
+	def run(self, edit, tag):
+		for region in self.view.sel():
+			text = self.view.substr(region)
+			self.view.replace(edit,region,"<"+tag+">"+text+"</"+tag.split()[0]+">")
+
